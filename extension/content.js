71252e86e0eeeb32c8bc708ae2d0d02c07aa9fbb
Ali Sabzevari <ali@tidm.ir>
2015-01-18 09:46:57 +0330

    Added cards count for every list

--- extension/content.js
+++ extension/content.js
@@ -2,8 +2,19 @@ $(document).ready(function () {
     setInterval(function() {
         modifyCardsInList();
         modifyCardDetails();
+        countListItems();
     }, 100);
 });
+
+function countListItems() {
+    $('.list').each(function(index) {
+        var outer = $(this);
+        $(this).find('h2.current').each(function (index) {
+            $(this).attr("title", $(this).text() + " (" + outer.find('.list-card').length + ")");
+            //console.log($(this).text() + ': ' + outer.find('.list-card').length);
+        });
+    });
+}
 
 function modifyCardsInList() {
     makeRtl('.list-card-title');
