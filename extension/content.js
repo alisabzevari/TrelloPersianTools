f671d30587edfe5fa440311a29a948fb29ffa686
Ali Sabzevari <ali@tidm.ir>
2015-01-21 13:35:56 +0330

    added ykh/trello-rtl-support code for better experience

--- extension/content.js
+++ extension/content.js
@@ -1,10 +1,73 @@
-$(document).ready(function () {
-    setInterval(function() {
-        // modifyCardsInList();
-        // modifyCardDetails();
-        countListItems();
-    }, 1000);
-});
+(function ($, document) {
+    var doms = 'h1, h2, h3, p ,a',
+        inputs = 'textarea, input[type=text]';
+
+    $(document).ajaxComplete(function () {
+        $(doms).each(function () {
+            updateStyle($(this));
+        });
+		countListItems();
+    });
+
+    $(document).ready(function () {
+        $(doms).each(function () {
+            updateStyle($(this));
+        });
+		countListItems();
+    });
+
+    $('body').on('input blur focus', inputs, function (e) {
+        updateStyle($(this));
+    });
+
+    function updateStyle(target) {
+        var regex = [],
+            matched,
+            value,
+            tagName = target[0].tagName,
+            rtl,
+            ltr;
+
+        rtl = {
+            'direction': 'rtl',
+            'text-align': 'right'
+        };
+        ltr = {
+            'direction': 'ltr',
+            'text-align': 'left'
+        };
+
+        value = target.text();
+
+        if (target.is('textarea') || target.is('input[type=text]')) {
+            value = target.val();
+        }
+
+        if (target.is('a') || tagName === 'H1' || tagName === 'H2' || tagName === 'H3') {
+            rtl['unicode-bidi'] = 'embed';
+            ltr['unicode-bidi'] = '';
+        }
+
+        // Persian, Arabic
+        regex.push(/[\u0600-\u06FF]/);
+        // Hebrew
+        regex.push(/[\u0590-\u05FF]/);
+
+        for (var i = 0; i < regex.length; i++) {
+            matched = value.match(regex[i]);
+
+            if (matched) {
+                break;
+            }
+        }
+
+        if (matched) {
+            target.css(rtl);
+        } else {
+            target.css(ltr);
+        }
+    }
+})($, document);
 
 function countListItems() {
     $('.list').each(function(index) {
@@ -14,21 +77,4 @@ function countListItems() {
             //console.log($(this).text() + ': ' + outer.find('.list-card').length);
         });
     });
-}
-
-function modifyCardsInList() {
-    makeRtl('.list-card-title');
-};
-
-function modifyCardDetails() {
-    makeRtl('.window-header');
-    makeRtl('.action-comment');
-    makeRtl('.checklist-item-details-text');
-}
-
-function makeRtl(selector) {
-    //$(selector).each(function() {
-    //    $(this).css('direction', 'rtl');
-    //});
-    $(selector).css('direction', 'rtl');
 }
